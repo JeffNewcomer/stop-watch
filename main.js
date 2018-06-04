61db0ff6a6aabcd765342c82931b62a460291b23
Jeff Newcomer <jrnewk@gmail.com>
2018-06-04 14:10:33 -0700

    Define a function that updates the elapsed seconds and writes it to the DOM. #1

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 document.querySelector('#startbutton').addEventListener("click", function() {
-  
+
+
 })
+var seconds = 0;
+
+function increaseSeconds( ) {
+  seconds = seconds +1
+  var updateCountOnSite = document.getElementById('secondCounter')
+  updateCountOnSite.textContent = seconds
+}
