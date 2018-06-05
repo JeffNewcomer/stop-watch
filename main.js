8bc63d98092656e5057be692083cdf714b200014
Jeff Newcomer <jrnewk@gmail.com>
2018-06-05 13:34:58 -0700

    Change the start button's background color to indicate the state of the watch. #2

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-// event listener place on the start button
-document.querySelector('#startbutton').addEventListener("click", function() {
-  // runs the increaseSeconds function once every second
-  setInterval(increaseSeconds, 1000)
+
+var stopWatchRunning = false
+var intervalID = null
+
+document.querySelector('#startbutton').addEventListener("click", function() {     // event listener place on the start button
+
+  if (stopWatchRunning === false){
+    stopWatchRunning = true
+    var interval = setInterval(increaseSeconds, 1000)                            // runs the increaseSeconds function once every second
+    intervalID = interval
+    document.querySelector('#startbutton').src = "pausewhite.png"; // pause
+  } else {
+    stopWatchRunning = false
+    clearInterval(intervalID)
+    intervalID = null
+    document.querySelector('#startbutton').src = "playblue.png"; //restart
+  }
 })
-// increases seconds in intervals of 1
+
 var seconds = 0;
-function increaseSeconds( ) {
+function increaseSeconds( ) {                                                      // increases seconds in intervals of 1
   seconds = seconds +1
-// updates the DOM with the current value of seconds
-  var updateCountOnSite = document.getElementById('secondCounter')
+  var updateCountOnSite = document.getElementById('secondCounter')                 // updates the DOM with the current value of seconds
   updateCountOnSite.textContent = seconds
 }
+
+//use an if then statement
+//use clearInterval
+//needs to flip from true to false?
+//onclick?
