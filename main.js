8bbfe1a496e3c926ec3a398c38f13b8a41b27480
Jeff Newcomer <jrnewk@gmail.com>
2018-06-06 13:27:48 -0700

    Update tick function to conditionally stop the timer. #4

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 
 var stopWatchRunning = false
 var intervalID = null
+var $input = document.getElementById('input')
 document.querySelector('#resetbutton').style.visibility = "hidden";               // hides the reset button
 
 document.querySelector('#startbutton').addEventListener("click", function() {     // event listener place on the start button
 
-  if (stopWatchRunning === false){
+  if (stopWatchRunning === false){                                               //  runs the timer
     stopWatchRunning = true
     var interval = setInterval(increaseSeconds, 1000)                            // runs the increaseSeconds function once every second
     intervalID = interval
     document.querySelector('#startbutton').src = "pausewhite.png";
     document.querySelector('#resetbutton').style.visibility = "visible";
-  } else {
+  } else {                                                                       // pauses the timer
     stopWatchRunning = false
     clearInterval(intervalID)
     intervalID = null
@@ -24,12 +25,24 @@ function increaseSeconds( ) {                                                   
   seconds = seconds +1
   var updateCountOnSite = document.getElementById('secondCounter')                 // updates the DOM with the current value of seconds
   updateCountOnSite.textContent = seconds
+
+  //timelimit feature
+
+  var inputValueToNumber = Number($input.value)
+
+  if (($input.value!=="") && (seconds === inputValueToNumber)){
+    stopWatchRunning = false
+    clearInterval(intervalID)
+    intervalID = null
+    document.querySelector('#startbutton').src = "playblue.png";
+  }
 }
 
 // code for the reset button function
 document.querySelector('#resetbutton').addEventListener("click", function() {
   if (stopWatchRunning === false){
   seconds = 0
+  $input.value = ""
   var updateCountOnSiteReset = document.getElementById('secondCounter')                 // updates the DOM with the current value of seconds
   updateCountOnSiteReset.textContent = seconds
   stopWatchRunning = false
